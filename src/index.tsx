c4aafe4d79de31c83b63c5953d457fd834f076bf
mrGreatProgrammer <fhgf9048@gmail.com>
2024-06-08 10:11:32 +0500

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate
+        loading={<h4>persistor Loading ...</h4>}
+        persistor={persistor}
+      >
+        <BrowserRouter>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorText: "#1f1f1f",
+              },
+            }}
+          >
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </ConfigProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
